631e09affd0866c3da7a01821c1e651be1c8ed5f
Matt Farina <matt@mattfarina.com>
2013-06-03 21:58:16 -0400

    Adding more docs and tests. The go version is accurate to many more decimal places than the php version.

--- earth.go
+++ earth.go
@@ -5,9 +5,11 @@ import (
 	"math"
 )
 
+// Earth contains information about the shape of the Earth.
 type Earth struct{}
 
 func (e *Earth) EarthRadiusSemimajor() float64 {
+	// The distance is in meters.
 	return 6378137.0
 }
 
@@ -16,6 +18,7 @@ func (e *Earth) EarthFlattening() float64 {
 }
 
 func (e *Earth) EarthEccentricitySq() float64 {
+	// The response is 2 decimal places more accurate than the PHP version.
 	return 2*e.EarthFlattening() - math.Pow(e.EarthFlattening(), 2)
 }
 
--- earth_test.go
+++ earth_test.go
@@ -11,4 +11,16 @@ func TestBasics(t *testing.T) {
 	if ret.EarthRadiusSemimajor() != 6378137.0 {
 		t.Error("! EarthRadiusSemimajor is reporting the wrong value.")
 	}
+
+	if ret.EarthEccentricitySq() != 0.006694379990141317 {
+		t.Error("! EarthEccentricitySq is reporting the wrong value.")
+	}
+
+	if ret.EarthRadiusSemiminor() != 6.356752314245179e+06 {
+		t.Error("! EarthRadiusSemiminor is reporting the wrong value.")
+	}
+
+	if ret.EarthFlattening() != 0.003352810664747481 {
+		t.Error("! EarthFlattening is reporting the wrong value.")
+	}
 }
