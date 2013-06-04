05c7a8487570011e50a0677ca42ec0e476fca9c6
Matt Farina <matt@mattfarina.com>
2013-06-03 21:33:39 -0400

    Added some more of the basics.

--- earth.go
+++ earth.go
@@ -1,8 +1,24 @@
 // Provides location information specific to Earch.
 package geo
 
+import (
+	"math"
+)
+
 type Earth struct{}
 
 func (e *Earth) EarthRadiusSemimajor() float64 {
 	return 6378137.0
+}
+
+func (e *Earth) EarthFlattening() float64 {
+	return 1 / 298.257223563
+}
+
+func (e *Earth) EarthEccentricitySq() float64 {
+	return 2*e.EarthFlattening() - math.Pow(e.EarthFlattening(), 2)
+}
+
+func (e *Earth) EarthRadiusSemiminor() float64 {
+	return (e.EarthRadiusSemimajor() * (1 - e.EarthFlattening()))
 }
