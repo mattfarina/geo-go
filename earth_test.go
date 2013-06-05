60f631a7183f74e289c954ccbe6ae69913801d0a
Matt Farina <matt@mattfarina.com>
2013-06-04 23:13:07 -0400

    Updating the tests to use a diff to a certain degree of accuracy as wonderfully recommended by Aaron France.

--- earth_test.go
+++ earth_test.go
@@ -4,23 +4,32 @@ import (
 	"testing"
 )
 
+const MAX_DIFF float64 = 0.0000001
+
+func diff(a, b float64) bool {
+    if a > b {
+        return (a - b) > MAX_DIFF
+    }
+    return (b - a) > MAX_DIFF
+}
+
 func TestBasics(t *testing.T) {
 
 	ret := new(Earth)
 
-	if ret.EarthRadiusSemimajor() != 6378137.0 {
+	if diff(ret.EarthRadiusSemimajor(), 6378137.0) {
 		t.Error("! EarthRadiusSemimajor is reporting the wrong value.")
 	}
 
-	if ret.EarthEccentricitySq() != 0.006694379990141317 {
+	if diff(ret.EarthEccentricitySq(), 0.006694379990141317) {
 		t.Error("! EarthEccentricitySq is reporting the wrong value.")
 	}
 
-	if ret.EarthRadiusSemiminor() != 6.356752314245179e+06 {
+	if diff(ret.EarthRadiusSemiminor(), 6.356752314245179e+06) {
 		t.Error("! EarthRadiusSemiminor is reporting the wrong value.")
 	}
 
-	if ret.EarthFlattening() != 0.003352810664747481 {
+	if diff(ret.EarthFlattening(), 0.003352810664747481) {
 		t.Error("! EarthFlattening is reporting the wrong value.")
 	}
 }
