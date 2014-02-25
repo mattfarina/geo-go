ab0223adc11dfee0a9c379a22918648e6bff9876
Matt Farina <matt@mattfarina.com>
2014-02-25 15:34:43 -0500

    Adding support to find the distance between two locations via the Vincenty Formulae.

--- location.go
+++ location.go
@@ -1,13 +1,27 @@
 package geo
 
+import (
+	"errors"
+	"github.com/Masterminds/convert/radial"
+	"math"
+)
+
 // Location is a location on the Earth.
-type Location struct{
+type Location struct {
 	Earth
 
 	// latitude, longitude, and height are where the location resides on the Earth.
-	latitude float64
+	latitude  float64
 	longitude float64
-	height float64
+	height    float64
+}
+
+// NewLocation creates a new Location based on a latitude and longitude.
+func NewLocation(lat, long float64) Location {
+	l := Location{}
+	l.SetLatitude(lat)
+	l.SetLongitude(long)
+	return l
 }
 
 // Latitude returns the latitude for this location on the Earth.
@@ -28,4 +42,78 @@ func (l *Location) Longitude() float64 {
 // SetLongitude sets the longitude for this location on the Earth.
 func (l *Location) SetLongitude(long float64) {
 	l.longitude = long
+}
+
+// func (l *Location) LatitudeRange(distance float64) (north, south Location) {
+
+// }
+
+// func (l *Location) LongitudeRange(distance float64) (east, west Location) {
+
+// }
+
+// Distance returns the distance, in meters, between two locations. One location
+// is this one and the second location is passed in. Vincenty's Forumlae is
+// used to calculate the distance. For more information see
+// https://en.wikipedia.org/wiki/Vincenty%27s_formulae
+func (l *Location) Distance(location Location) (float64, error) {
+	lat1 := radial.DegToRad(l.Latitude())
+	long1 := radial.DegToRad(l.Longitude())
+	lat2 := radial.DegToRad(location.Latitude())
+	long2 := radial.DegToRad(location.Longitude())
+
+	a := l.EarthRadiusSemimajor()
+	b := l.EarthRadiusSemiminor()
+	f := l.EarthFlattening()
+
+	la := long2 - long1
+	lambda := la
+	lambdaP := 2 * math.Pi
+
+	u1 := math.Atan((1 - f) * math.Tan(lat1))
+	u2 := math.Atan((1 - f) * math.Tan(lat2))
+
+	sinU1 := math.Sin(u1)
+	cosU1 := math.Cos(u1)
+	sinU2 := math.Sin(u2)
+	cosU2 := math.Cos(u2)
+
+	var sinLambda, cosLambda, sinSigma, cosSigma, sigma, alpha, cosSqAlpha float64
+	var cos2SigmaM, c float64
+
+	iterLimit := 20
+
+	for ; (math.Abs(lambda-lambdaP) > 1e-12) && iterLimit > 0; iterLimit-- {
+		sinLambda = math.Sin(lambda)
+		cosLambda = math.Cos(lambda)
+		sinSigma = math.Sqrt((cosU2*sinLambda)*(cosU2*sinLambda) + (cosU1*sinU2-sinU1*cosU2*cosLambda)*(cosU1*sinU2-sinU1*cosU2*cosLambda))
+
+		if sinSigma == 0 {
+			return 0, nil
+		}
+
+		cosSigma = sinU1*sinU2 + cosU1*cosU2*cosLambda
+		sigma = math.Atan2(sinSigma, cosSigma)
+		alpha = math.Asin(cosU1 * cosU2 * sinLambda / sinSigma)
+		cosSqAlpha = math.Cos(alpha) * math.Cos(alpha)
+		cos2SigmaM = cosSigma - 2*sinU1*sinU2/cosSqAlpha
+		c = f / 16 * cosSqAlpha * (4 + f*(4-3*cosSqAlpha))
+		lambdaP = lambda
+		lambda = la + (1-c)*f*math.Sin(alpha)*(sigma+c*sinSigma*(cos2SigmaM+c*cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)))
+	}
+
+	// This would mean we have an error
+	if iterLimit == 0 {
+		return 0, errors.New("Geo: Error calculating distance.")
+	}
+
+	uSq := cosSqAlpha * (a*a - b*b) / (b * b)
+	A := 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)))
+	B := uSq / 1024 * (256 + uSq*(-128+uSq*(74-47*uSq)))
+
+	deltaSigma := B * sinSigma * (cos2SigmaM + B/4*(cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)-B/6*cos2SigmaM*(-3+4*sinSigma*sinSigma)*(-3+4*cos2SigmaM*cos2SigmaM)))
+
+	s := b * A * (sigma - deltaSigma)
+
+	return s, nil
 }
--- location_test.go
+++ location_test.go
@@ -36,3 +36,34 @@ func TestLongitude(t *testing.T) {
 		t.Error("! The longitude was not set or retrievable for the location.")
 	}
 }
+
+func TestDistance(t *testing.T) {
+	google := NewLocation(37.422045, -122.084347) // Google HQ
+	sf := NewLocation(37.77493, -122.419416)      // San Fransisco, CA
+	ef := NewLocation(48.8582, 2.294407)          // Eiffel Tower
+	opera := NewLocation(-33.856553, 151.214696)  // Sydney Opera House
+
+	distance, err := google.Distance(sf)
+	if err != nil {
+		t.Error("! Error calculating the distance between Google and San Francisco")
+	}
+	if diff(distance, 49087.0658381) {
+		t.Error("! An incorrect distance was calculated between Google and San Francisco", distance)
+	}
+
+	distance, err = google.Distance(ef)
+	if err != nil {
+		t.Error("! Error calculating the distance between Google and the Eiffel Tower")
+	}
+	if diff(distance, 8989724.3991584) {
+		t.Error("! An incorrect distance was calculated between Google and the Eiffel Tower", distance)
+	}
+
+	distance, err = google.Distance(opera)
+	if err != nil {
+		t.Error("! Error calculating the distance between Google and the Sydney Opera House")
+	}
+	if diff(distance, 11939773.6402807) {
+		t.Error("! An incorrect distance was calculated between Google and the Sydney Opera House", distance)
+	}
+}
